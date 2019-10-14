71cbd5d209331c89fdf8f359353d7ffde81b4060
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2019-10-14 19:50:23 +0800

    Updates

--- precache-manifest.c66ca5b1acd36d493e9b050928890bcc.js
+++ precache-manifest.c66ca5b1acd36d493e9b050928890bcc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d015eea06a71271b21b2805fc9ab853",
+    "revision": "9709a950d1a1d90ad8b843f8d4a859ad",
     "url": "/react-Todos/index.html"
   },
   {
-    "revision": "a17ac7a42b7225cbbb77",
-    "url": "/react-Todos/static/css/main.d27f47ef.chunk.css"
+    "revision": "b1e08e3e17b98d07ca2a",
+    "url": "/react-Todos/static/css/main.111767f5.chunk.css"
   },
   {
     "revision": "c99cce51f18ddf7656cd",
     "url": "/react-Todos/static/js/2.a615f97d.chunk.js"
   },
   {
-    "revision": "a17ac7a42b7225cbbb77",
-    "url": "/react-Todos/static/js/main.39348746.chunk.js"
+    "revision": "b1e08e3e17b98d07ca2a",
+    "url": "/react-Todos/static/js/main.48c1ef2d.chunk.js"
   },
   {
     "revision": "5eb478b8369a88e0585f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-Todos/precache-manifest.d1b48dfdc18bab12fced78a9cde1762a.js"
+  "/react-Todos/precache-manifest.c66ca5b1acd36d493e9b050928890bcc.js"
 );
 
 self.addEventListener('message', (event) => {
